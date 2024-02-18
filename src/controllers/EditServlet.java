6878db513697567557b57d94c81773b699dccc6f
t-shidori <t-shidori@FnSales-NOTE01>
2024-02-19 03:22:43 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -44,8 +44,12 @@ public class EditServlet extends HttpServlet {
 
         // メッセージIDをセッションスコープに登録
         request.getSession().setAttribute("message_id", m.getId());
+        
+        if(m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
-}
+}   
